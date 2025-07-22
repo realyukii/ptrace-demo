6012c03bc8de3842bb313df5064dce9e1373fe34
ReYuki <me@reyuki.site>
2025-07-22 17:38:34 +0700

    test: read fixed, hard-coded memory of a process
    Co-authored-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- tracer.c
+++ tracer.c
@@ -32,6 +32,33 @@ static void signal_handler(int c)
 	__sys_ptrace(PTRACE_DETACH, global_cfg->pid, NULL, NULL);
 }
 
+#define copy_from_process(pid, dst, src, len) \
+	__copy_from_process((pid), (dst), (const void *)(src), (len))
+
+static ssize_t __copy_from_process(int pid, void *dst, const void *src, size_t len)
+{
+	const char *srcp = src;
+	char *dstp = dst;
+
+	while (len > 0) {
+		size_t cp_len;
+		ssize_t ret;
+		long tmp;
+
+		ret = __sys_ptrace(PTRACE_PEEKTEXT, pid, (void *)srcp, &tmp);
+		if (ret < 0)
+			return ret;
+
+		cp_len = (len > sizeof(tmp)) ? sizeof(tmp) : len;
+		memcpy(dstp, &tmp, cp_len);
+		srcp += cp_len;
+		dstp += cp_len;
+		len -= cp_len;
+	}
+
+	return 0;
+}
+
 static void tracer_handler(int child_pid)
 {
 	ZydisDisassembledInstruction instruction;
@@ -59,53 +86,65 @@ static void tracer_handler(int child_pid)
 		/*
 		* we don't interested on signal beyond debugging (e.g SIGWINCH)
 		*/
-		if (WSTOPSIG(wstatus) != SIGTRAP
-		&& WSTOPSIG(wstatus) != SIGSTOP)
-			continue;
+		// if (WSTOPSIG(wstatus) != SIGTRAP
+		// && WSTOPSIG(wstatus) != SIGSTOP)
+		// 	continue;
 
 		__sys_ptrace(
 			PTRACE_GETREGS, child_pid, NULL, &regs
 		);
 
-		insn[0] = 0;
-		ret = __sys_ptrace(
-			PTRACE_PEEKTEXT, child_pid,
-			(void *)regs.rip, &insn[0]
-		);
-		assert(!ret);
+		copy_from_process(child_pid, insn, regs.rip, 4);
 
-		insn[1] = 0;
-		ret = __sys_ptrace(
-			PTRACE_PEEKTEXT, child_pid,
-			(void *)regs.rip + sizeof(long), &insn[1]
-		);
-		assert(!ret);
-
-		ret = ZydisDisassembleIntel(
-				ZYDIS_MACHINE_MODE_LONG_64, regs.rip, insn,
-				sizeof(insn), &instruction
-		);
-		if (ZYAN_SUCCESS(ret)) {
-			uint8_t len = instruction.info.length;
-
-			printf(
-				"%p: %s (instruction length %d) ",
-				(void *)regs.rip, instruction.text, len
-			);
-
-			for (size_t i = 0; i < len; i++) {
-				uint8_t *ptr = (void *)insn;
-				printf("0x%02x ", ptr[i]);
-			}
-
-			puts("");
-
-		} else {
-			/* unlikely */
-			fprintf(stderr, "error occured\n");
-			__sys_ptrace(PTRACE_DETACH, child_pid, NULL, NULL);
-			break;
+		if (!memcmp(insn, "\x83\x45\xc8\x01", 4)) {
+			int counter;
+			copy_from_process(child_pid, &counter, regs.rbp-0x38, sizeof(counter));
+			printf("counter incremented: %d!\n", counter);
 		}
+
+		// insn[0] = 0;
+		// ret = __sys_ptrace(
+		// 	PTRACE_PEEKTEXT, child_pid,
+		// 	(void *)regs.rip, &insn[0]
+		// );
+		// assert(!ret);
+
+		// insn[1] = 0;
+		// ret = __sys_ptrace(
+		// 	PTRACE_PEEKTEXT, child_pid,
+		// 	(void *)regs.rip + sizeof(long), &insn[1]
+		// );
+		// assert(!ret);
+
+		// ret = ZydisDisassembleIntel(
+		// 		ZYDIS_MACHINE_MODE_LONG_64, regs.rip, insn,
+		// 		sizeof(insn), &instruction
+		// );
+		// if (ZYAN_SUCCESS(ret)) {
+		// 	uint8_t len = instruction.info.length;
+
+		// 	printf(
+		// 		"%p: %s (instruction length %d) ",
+		// 		(void *)regs.rip, instruction.text, len
+		// 	);
+
+		// 	for (size_t i = 0; i < len; i++) {
+		// 		uint8_t *ptr = (void *)insn;
+		// 		printf("0x%02x ", ptr[i]);
+		// 	}
+
+		// 	puts("");
+
+		// } else {
+		// 	/*
+		// 	* unlikely.
+		// 	* for error interpretation, see:
+		// 	* https://github.com/zyantific/zydis/blob/master/tools/ZydisToolsShared.c#L79
+		// 	*/
+		// 	fprintf(stderr, "error occured: %d\n", ZYAN_STATUS_CODE(ret));
+		// 	__sys_ptrace(PTRACE_DETACH, child_pid, NULL, NULL);
+		// 	break;
+		// }
 
 		__sys_ptrace(PTRACE_SINGLESTEP, child_pid, NULL, NULL);
 	}
